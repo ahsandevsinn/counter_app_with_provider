3d984f21f643fce0ace12f7b9ade2a76d1df07c9
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-09-07 23:51:58 +0500

    final code

---
+++ counter_app_with_provider/counter_app_with_riverpod/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:counter_app_with_riverpod/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(ProviderScope(child:  MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+     
+      home: HomeScreen()
+    );
+  }
+}
+
